628289c0a0d3aea580c4428d70ae40170a78b97e
lintong <lintong@oxfordrobotics.institute>
2023-03-14 10:24:04 +0000

    use camera info from undistorted image

--- apriltag_ros/include/apriltag_ros/continuous_detector.h
+++ apriltag_ros/include/apriltag_ros/continuous_detector.h
@@ -60,7 +60,8 @@ class ContinuousDetector: public nodelet::Nodelet
 
   void onInit();
 
-  void imageCallback(const sensor_msgs::ImageConstPtr& image_rect);
+  void imageCallback(const sensor_msgs::ImageConstPtr& image_rect,
+                     const sensor_msgs::CameraInfoConstPtr& camera_info);
 
  private:
   std::shared_ptr<TagDetector> tag_detector_;
@@ -69,7 +70,6 @@ class ContinuousDetector: public nodelet::Nodelet
 
   std::shared_ptr<image_transport::ImageTransport> it_;
   image_transport::CameraSubscriber camera_image_subscriber_;
-  image_transport::Subscriber image_subscriber_;
   image_transport::Publisher tag_detections_image_publisher_;
   ros::Publisher tag_detections_publisher_;
 };
